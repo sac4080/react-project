e8872960317729f3264a1ed655f50dfdeb883eae
Sachin99 <31537607+sac4080@users.noreply.github.com>
2020-11-16 15:18:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,8 +10,12 @@ class App extends Component{
   render(){
     return(
       <div classname="App">
+        <div className="container">
+        <h1 className="title">Latest pics Of the day</h1>
         <PostList />
-        
+      </div>
+
+
 
       </div>
     )
