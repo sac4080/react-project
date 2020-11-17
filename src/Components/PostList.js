721082a681bb809f520a26b581dfd47400d03246
Sachin99 <31537607+sac4080@users.noreply.github.com>
2020-11-17 19:41:20 +0530

    Update PostList.js

--- src/Components/PostList.js
+++ src/Components/PostList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
 import Unsplash, { toJson } from 'unsplash-js';
+
 import Masonry from 'react-masonry-component';
 
 const unsplash = new Unsplash({ accessKey: "t56lYPrZgJLZjLrhC7WF3ZpXnQ4Bh1ppbU_IZmQWXZw",
@@ -15,12 +16,27 @@ class  PostList extends Component{
         super(props)
         this.state={
             posts:[{"urls":"lol"}],
-            loaded:false
+            loaded:false,
+            showModal:0
         }
+        this.handleClick = this.handleClick.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+    }
+
+    handleClick(id) {
+        this.setState({
+            showModal:id
+        });
+    }
+
+    handleClose() {
+        this.setState({
+            showModal:-1
+        })
     }
 
     componentDidMount(){
-        unsplash.photos.listPhotos(1, 100, "latest")
+        unsplash.photos.listPhotos(1, 1000, "latest")
         .then(toJson)
         .then(json => {
          console.log(json);
@@ -28,28 +44,49 @@ class  PostList extends Component{
                 posts:json,
                 loaded:true
         })
+
     });
-
-
-
+    
     }
+    
     render(){
 
         const childElements = this.state.posts.map(function(element){
 
             return (
-              <div className="column">
+              <div className="column" >
                 <div className="row">
-                  <div className="myImg">
-                   <li>
-                     <img src={element.urls.regular}
-                     alt={element.alt_description}
-                     title={element.alt_description}/>
-                     
-                    </li>
-              </div></div></div>
+                    
+                        <div className="myImg">
+                         <li>
+                            <img src={element.urls.regular}
+                            alt={element.alt_description}
+                            title={element.alt_description}
+                            onClick={() => this.handleClick(element.id)}
+                            />
+                            {
+                                this.state.showModal===element.id && (
+                                    <dialog
+                                        className="dialog"
+                                        open
+                                        style={{zIndex:10 , position:'absolute', top:0,left:0, backgroundColor:'black'}}
+                                        onClick={this.handleClose}
+                                    >
+                                        <img src={element.urls.regular}
+                                        alt={element.alt_description}
+                                        title={element.alt_description}
+                                        style={{width:500, height:500}}
+                                        />
+                                        
+                                    </dialog>
+                                )
+                            }
+                        </li>
+                        </div>
+                  
+              </div></div>
              );
-         });
+         }.bind(this));
 
          if(!this.state.loaded)
             return(
@@ -57,7 +94,6 @@ class  PostList extends Component{
                     loading ....
                 </div>
             )
-
 
         return(
             <div>
